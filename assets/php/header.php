ec1dad4efc89ef42af4495dce3c4dd1143d2361d
1103Maheshwaran <g.maheshwaran2015@vit.ac.in>
2020-11-25 07:45:13 +0530

    Testing First Commit

---
+++ assets/php/header.php
@@ -0,0 +1,15 @@
+<header id="header">
+	<div class="inner">
+    <!-- Logo -->
+		<a href="index.php" class="logo">
+            <span class="symbol"><img src="images/muruga.jpg" alt="" /></span>
+            <span class="title">Sthanunanadha</span>
+		</a>            
+        <!-- Nav -->
+        <nav>
+		    <ul>
+				<li><a href="#menu">Menu</a></li>
+			</ul>
+		</nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Subramanya Bhajanamrutham</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<noscript>
+		<link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+</head>
+
+<body class="is-preload">
+	<!-- Wrapper -->
+	<div id="wrapper">
+
+		<!-- Header -->
+		<?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
+		include($IPATH . "header.php"); ?>
+
+		<!-- Menu -->
+		<nav id="menu">
+			<h2>Menu</h2>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about_composer.php">About Composer</a></li>
+				<li><a href="gallery.php">Gallery</a></li>
+				<li><a href="singer.php">Singers</a></li>
+			</ul>
+		</nav>
+
+		<!-- Main -->
+		<div id="main">
+			<div class="inner">
+				<header>
+					<h1>Sree Subramanya Bhajanamrutham<br /></h1>
+					<h2>Sthanunanadha, The pride of Nagarkovil.</h2>
+				</header>
+				<section class="tiles">
+					<article class="style1">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="thodaka_mangalam.php">
+							<h2>Thodaka Mangalam</h2>
+							<div class="content">
+								<p>The starting Mangalam.</p>
+							</div>
+						</a>
+					</article>
+					<article class="style2">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="deepaaraadhanam.php">
+							<h2>Deepaaraadhanam</h2>
+							<div class="content">
+								<p>Devotional Songs.</p>
+							</div>
+						</a>
+					</article>
+					<article class="style3">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="divyanaamam.php">
+							<h2>Divyanaamam</h2>
+							<div class="content">
+								<p>Devotional Songs.</p>
+							</div>
+						</a>
+					</article>
+					<article class="style4">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="deepa_pradakshinam.php">
+							<h2>Deepa Pradakshinam</h2>
+							<div class="content">
+								<p>Devotional Songs.</p>
+							</div>
+						</a>
+					</article>
+					<article class="style5">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="dolothsavam.php">
+							<h2>Dolothsavam</h2>
+							<div class="content">
+								<p>Devotional Songs.</p>
+							</div>
+						</a>
+					</article>
+					<article class="style6">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="mangalam.php">
+							<h2>Mangalam</h2>
+							<div class="content">
+								<p>Ending Mangalam.</p>
+							</div>
+						</a>
+					</article>
+					<article class="style7">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="keerthanas.php">
+							<h2>Keerthanas</h2>
+							<div class="content">
+								<p>Devotional Songs.</p>
+							</div>
+						</a>
+					</article>
+					<article class="style8">
+						<span class="image">
+							<img src="images/subuu.jpg" alt="" />
+						</span>
+						<a href="namavalees.php">
+							<h2>Namavalees</h2>
+							<div class="content">
+								<p>Devotional Songs.</p>
+							</div>
+						</a>
+					</article>
+				</section>
+			</div>
+		</div>
+
+		<!-- Footer -->
+		<?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
+		include($IPATH . "foot.php"); ?>
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+</body>
+
+</html>
