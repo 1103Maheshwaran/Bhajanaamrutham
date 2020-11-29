797bb0985a1ec81f250c3c233a098c74fa072b17
1103Maheshwaran <g.maheshwaran2015@vit.ac.in>
2020-11-29 22:03:38 +0530

    PageLoader added

--- index.php
+++ index.php
@@ -23,136 +23,143 @@
 		<link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 </head>
 
-<body class="is-preload">
-	<!-- Wrapper -->
-	<div id="wrapper">
+<body>
+	<div class="startload">
+		<img class="om" src="images/om.ico" alt="om" />
+		<h3>Loading...</h3>
+	</div>
+	<div class="is-preload">
+		<!-- Wrapper -->
+		<div id="wrapper">
 
-		<!-- Header -->
-		<?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
-		include($IPATH . "header.php"); ?>
+			<!-- Header -->
+			<?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
+			include($IPATH . "header.php"); ?>
 
-		<!-- Menu -->
-		<nav id="menu">
-			<h2>Menu</h2>
-			<ul>
-				<li><a href="index">Home</a></li>
-				<li><a href="about_composer">About Composer</a></li>
-				<li><a href="gallery">Gallery</a></li>
-				<li><a href="singer">Singers</a></li>
-			</ul>
-		</nav>
+			<!-- Menu -->
+			<nav id="menu">
+				<h2>Menu</h2>
+				<ul>
+					<li><a href="index">Home</a></li>
+					<li><a href="about_composer">About Composer</a></li>
+					<li><a href="gallery">Gallery</a></li>
+					<li><a href="singer">Singers</a></li>
+				</ul>
+			</nav>
 
-		<!-- Main -->
-		<div id="main">
-			<div class="inner">
-				<header>
-					<h1>Sree Subrahmanya Bhajanaamrutham<br /></h1>
-					<h2></h2>
-				</header>
-				<section class="tiles">
-					<article class="style1">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="thodaka_mangalam">
-							<h2>Thodaka Mangalam</h2>
-							<div class="content">
-								<p>The starting Mangalam.</p>
-							</div>
-						</a>
-					</article>
-					<article class="style2">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="deepaaraadhanam">
-							<h2>Deepaaraadhanam</h2>
-							<div class="content">
-								<p>Devotional Songs.</p>
-							</div>
-						</a>
-					</article>
-					<article class="style3">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="divyanaamam">
-							<h2>Divyanaamam</h2>
-							<div class="content">
-								<p>Devotional Songs.</p>
-							</div>
-						</a>
-					</article>
-					<article class="style4">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="deepa_pradakshinam">
-							<h2>Deepa Pradakshinam</h2>
-							<div class="content">
-								<p>Devotional Songs.</p>
-							</div>
-						</a>
-					</article>
-					<article class="style5">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="dolothsavam">
-							<h2>Dolothsavam</h2>
-							<div class="content">
-								<p>Devotional Songs.</p>
-							</div>
-						</a>
-					</article>
-					<article class="style6">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="mangalam">
-							<h2>Mangalam</h2>
-							<div class="content">
-								<p>Ending Mangalam.</p>
-							</div>
-						</a>
-					</article>
-					<article class="style7">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="Keerthanas">
-							<h2>Keerthanas</h2>
-							<div class="content">
-								<p>Devotional Songs.</p>
-							</div>
-						</a>
-					</article>
-					<article class="style8">
-						<span class="image">
-							<img src="images/subuu.jpg" alt="" />
-						</span>
-						<a href="namavalees">
-							<h2>Namavalees</h2>
-							<div class="content">
-								<p>Devotional Songs.</p>
-							</div>
-						</a>
-					</article>
-				</section>
+			<!-- Main -->
+			<div id="main">
+				<div class="inner">
+					<header>
+						<h1>Sree Subrahmanya Bhajanaamrutham<br /></h1>
+						<h2></h2>
+					</header>
+					<section class="tiles">
+						<article class="style1">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="thodaka_mangalam">
+								<h2>Thodaka Mangalam</h2>
+								<div class="content">
+									<p>The starting Mangalam.</p>
+								</div>
+							</a>
+						</article>
+						<article class="style2">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="deepaaraadhanam">
+								<h2>Deepaaraadhanam</h2>
+								<div class="content">
+									<p>Devotional Songs.</p>
+								</div>
+							</a>
+						</article>
+						<article class="style3">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="divyanaamam">
+								<h2>Divyanaamam</h2>
+								<div class="content">
+									<p>Devotional Songs.</p>
+								</div>
+							</a>
+						</article>
+						<article class="style4">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="deepa_pradakshinam">
+								<h2>Deepa Pradakshinam</h2>
+								<div class="content">
+									<p>Devotional Songs.</p>
+								</div>
+							</a>
+						</article>
+						<article class="style5">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="dolothsavam">
+								<h2>Dolothsavam</h2>
+								<div class="content">
+									<p>Devotional Songs.</p>
+								</div>
+							</a>
+						</article>
+						<article class="style6">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="mangalam">
+								<h2>Mangalam</h2>
+								<div class="content">
+									<p>Ending Mangalam.</p>
+								</div>
+							</a>
+						</article>
+						<article class="style7">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="Keerthanas">
+								<h2>Keerthanas</h2>
+								<div class="content">
+									<p>Devotional Songs.</p>
+								</div>
+							</a>
+						</article>
+						<article class="style8">
+							<span class="image">
+								<img src="images/subuu.jpg" alt="" />
+							</span>
+							<a href="namavalees">
+								<h2>Namavalees</h2>
+								<div class="content">
+									<p>Devotional Songs.</p>
+								</div>
+							</a>
+						</article>
+					</section>
+				</div>
 			</div>
+
+			<!-- Footer -->
+			<?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
+			include($IPATH . "foot.php"); ?>
 		</div>
 
-		<!-- Footer -->
-		<?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/assets/php/";
-		include($IPATH . "foot.php"); ?>
+		<!-- Scripts -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/browser.min.js"></script>
+		<script src="assets/js/breakpoints.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
 	</div>
-
-	<!-- Scripts -->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/browser.min.js"></script>
-	<script src="assets/js/breakpoints.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<script src="assets/js/main.js"></script>
+	<script src="app.js"></script>
 </body>
 
 </html>
